a818a8e1a90abdbcf3e3cb7377d79ded29d0f61e
Gaal Yahas <gaal@forum2.org>
2012-02-10 16:06:43 +0200

    prolix-go 0.02

--- prolix.go
+++ prolix.go
@@ -52,7 +52,7 @@ import (
 	"github.com/gaal/go-options/options"
 )
 
-const versionString = "0.01-go"
+const versionString = "0.02-go"
 
 const timestampFormat = "20060102T150405" // yyyyMMddThhmmss in localtime.
 
